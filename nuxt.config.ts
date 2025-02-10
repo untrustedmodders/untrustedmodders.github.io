9dc44ff1c037c367c6eb411078f7ff998af5074e
Fr4nch <golikov12super@gmail.com>
2025-02-11 02:31:40 +0300

    mini update langs cards, add highlight support for langs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,14 @@ export default defineNuxtConfig({
     static: true,
   },
   compatibilityDate: '2024-07-06',
+  content: {
+    documentDriven: true,
+    highlight: {
+      theme: {
+        default: 'github-light',
+        dark: 'github-dark',
+      },
+      preload: ['json', 'js', 'ts', 'html', 'css', 'vue', 'diff', 'shell', 'markdown', 'yaml', 'bash', 'ini', 'python', 'cpp', 'csharp', 'go', 'cmake'],
+    },
+  },
 });
